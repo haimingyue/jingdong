c6a4c93726a9660e2d1f7cfaf7ba69e45c04b888
simoon <420526391@qq.com>
2019-06-07 16:58:05 +0800

    init

---
+++ app/js/home/index.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div :class="$style.home">
+    <h1>Home
+
+      <div class="title">title</div>
+    </h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" module>
+  @import '../../css/reset.scss';
+  .home {
+    color: red;
+    font-size: 80px;
+  }
+</style>
